01a19350b2fafcb9a8ca39ff0e801ff5b3f7e734
Nasukhi04 <a29109902@gmail.com>
2026-03-02 13:34:54 +0700

    menambahkan file tambah_data.php

---
+++ tambah_data.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Tambah Data Barang</title>
+</head>
+<body>
+
+    <h2>Form Tambah Data Barang</h2>
+
+    <form method="POST" action="proses_data.php">
+
+        <label>Nama Barang:</label><br>
+        <input type="text" name="nama_barang" required><br><br>
+
+        <label>Kategori:</label><br>
+        <select name="kategori" required>
+            <option value="">-- Pilih Kategori --</option>
+            <option value="Elektronik">Elektronik</option>
+            <option value="ATK">ATK</option>
+        </select><br><br>
+
+        <label>Stok:</label><br>
+        <input type="number" name="stok" min="0" required><br><br>
+
+        <label>Harga:</label><br>
+        <input type="number" name="harga" min="0" required><br><br>
+
+        <button type="submit">Simpan Data</button>
+
+    </form>
+
+</body>
+</html>
